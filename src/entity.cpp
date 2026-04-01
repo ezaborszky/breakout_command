a212e93bd36f2efdc0ed159f682aa8c656dc2e7f
ezaborszky <ede.zaborszky@gmail.com>
2026-04-01 21:54:02 +0200

    added set-get funcs

---
+++ src/entity.cpp
@@ -0,0 +1,26 @@
+#include "Entity.h"
+#include <utility>
+
+void Entity::setPosition(float x, float y) { transform_->position = {x, y}; }
+
+void Entity::setDirection(float x, float y) {
+  transform_->directionVector = {x, y};
+}
+
+void Entity::setSpeed(float speed) { transform_->speed = speed; }
+
+void Entity::setState(State state) { state_->state = state; }
+
+std::pair<float, float> Entity::getPosition() const {
+  return transform_->position;
+}
+
+std::pair<float, float> Entity::getDirection() const {
+  return transform_->directionVector;
+}
+
+float Entity::getSpeed() const { return transform_->speed; }
+
+State Entity::getState() const { return state_->state; }
+
+void Entity::destroy() { active_ = false; }
