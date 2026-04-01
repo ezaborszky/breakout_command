5c3fe93d96f1e9ebd79b8d3600a44815d6a0957c
ezaborszky <ede.zaborszky@gmail.com>
2026-04-01 21:26:11 +0200

    set up functions

--- src/Entity.h
+++ src/Entity.h
@@ -1,10 +1,22 @@
 #pragma once
 #include "Component.h"
 #include <memory>
+#include <utility>
 class Entity {
 public:
-  Entity() {}
-  ~Entity() {}
+  Entity();
+  ~Entity();
+
+  void setPosition(float x, float y);
+  void setDirection(float x, float y);
+  void setSpeed(float speed);
+  void setState(State state);
+  std::pair<float, float> getPosition() const;
+  std::pair<float, float> getDirection() const;
+  float getSpeed() const;
+  State getState() const;
+
+private:
   std::unique_ptr<CTransform> transform_ = nullptr;
   std::unique_ptr<CState> state_ = nullptr;
 };
