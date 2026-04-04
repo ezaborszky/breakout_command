cc0fe0aa11012461353cfbdd3629dfd2625e7cec
ezaborszky <ede.zaborszky@gmail.com>
2026-04-04 18:40:35 +0200

    detect cwd

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,10 @@
 #include "Game.h"
 #include <iostream>
+#include <libgen.h>
+#include <unistd.h>
 
-int main(int argc, char *args[]) {
+int main(int argc, char *argv[]) {
+  chdir(dirname(argv[0]));
   std::cout << "Hello, világ!\n";
   Game g;
   g.run();
