6d887d01272b0a8d2156cc7b9012f9d6d6729cdd
ezaborszky <ede.zaborszky@gmail.com>
2026-04-05 17:18:01 +0200

    linked EC system

--- compile.sh
+++ compile.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-g++ ./src/main.cpp ./src/game.cpp ./src/engine.cpp -lSDL2 -lSDL2_image -o ./build/main
+g++ ./src/main.cpp ./src/game.cpp ./src/engine.cpp ./src/entitymanager.cpp ./src/entity.cpp ./src/utility.cpp -lSDL2 -lSDL2_image -o ./build/main
 echo "Build complete!"
