39e3aa40dda3a10d0302987ce2431a0ea3e4b955
ezaborszky <ede.zaborszky@gmail.com>
2026-04-05 17:18:47 +0200

    cleaned up main

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,9 @@
 #include "Game.h"
-#include <iostream>
 #include <libgen.h>
 #include <unistd.h>
 
 int main(int argc, char *argv[]) {
   chdir(dirname(argv[0]));
-  std::cout << "Hello, világ!\n";
   Game g;
   g.run();
   return 0;
